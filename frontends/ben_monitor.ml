tanglu-org/ben

commit 56999aa8a9abc6561ad6b35492167d369b864ca1

Do not use "Binary" field of source packages
It is too imprecise and can confuse the monitor when binary packages
with the same name are produced from different source packages. Use
exclusively source package information available in binary packages.

frontends/ben_monitor.ml
@@ -480,16 +480,21 @@ let compute_monitor_data sources binaries rounds =
     let packages = List.sort (fun x y -> compare !!!x !!!y) xs in
     List.map begin fun sname ->
       let src = M.find sname sources in
+      let src_name = Package.get "package" src in
+      let src_version = Package.get "version" src in
       let states =
         List.map begin fun arch ->
-          let pkgs = Package.binaries src in
-          List.fold_left begin fun accu pkg ->
-            try
-              let pkg = PAMap.find (pkg, arch) binaries in
+          (* FIXME: indexing by name+arch is not a good idea after all *)
+          PAMap.fold (fun (_, arch') pkg accu ->
+            if arch' = arch &&
+              Package.get "source" pkg = src_name &&
+              Package.get "source-version" pkg = src_version
+            then
               let state = compute_state pkg in
               combine_states accu state
-            with Not_found -> accu
-          end Unknown pkgs
+            else
+              accu
+          ) binaries Unknown
         end !Benl_clflags.architectures
       in src, states
     end packages
@@ -830,16 +835,6 @@ let compute_graph () =
          M.add name (Package.Name.of_string source) accu)
       binaries
       M.empty
-  in
-  let src_of_bin =
-    M.fold
-      (fun name pkg accu ->
-         List.fold_left
-           (fun accu bin -> M.add bin name accu)
-           accu
-           (Package.binaries pkg))
-      sources
-      src_of_bin
   in
   let dep_graph = Dependencies.get_dep_graph sources src_of_bin in
   let rounds = Dependencies.topo_split dep_graph in

lib/package.ml
@@ -108,9 +108,6 @@ let get_and_split =
 let build_depends x =
   get_and_split "build-depends-indep" x @ get_and_split "build-depends" x
 
-let binaries x =
-  get_and_split "binary" x
-
 type dependency = {
   dep_name : string;
   dep_version : (comparison * string) option;

lib/package.mli
@@ -62,7 +62,6 @@ module Map : sig
 end
 
 val build_depends : [`source] t -> [`binary] Name.t list
-val binaries : [`source] t -> [`binary] Name.t list
 
 type dependency = {
   dep_name : string;

commit 6a3e02ebc3fcbbc8c7c524c4ce246f8a31295bd5

Checking for "source-version" when computing states is wrong.
Fixes regression introduced in 56999aa8a9abc6561ad6b35492167d369b864ca1

When surch check is performed, packages from same source package with
older source version are showed as "Unknown" instead of "Bad".

frontends/ben_monitor.ml
@@ -481,14 +481,12 @@ let compute_monitor_data sources binaries rounds =
     List.map begin fun sname ->
       let src = M.find sname sources in
       let src_name = Package.get "package" src in
-      let src_version = Package.get "version" src in
       let states =
         List.map begin fun arch ->
           (* FIXME: indexing by name+arch is not a good idea after all *)
           arch, PAMap.fold (fun (_, arch') pkg accu ->
             if arch' = arch &&
-              Package.get "source" pkg = src_name &&
-              Package.get "source-version" pkg = src_version
+              Package.get "source" pkg = src_name
             then
               let state = compute_state pkg in
               combine_states accu state